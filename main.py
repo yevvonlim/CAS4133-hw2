4d0b575be96313ee1bba0f65f8dbb267a0e235c4
Yewon Lim <ga06033@yonsei.ac.kr>
2025-05-22 06:28:03 +0000

    Add project files under Git LFS

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def main():
-    print("Hello from cas4133!")
-
-
-if __name__ == "__main__":
-    main()
+version https://git-lfs.github.com/spec/v1
+oid sha256:6dfefc3af4aa708035304f3e40ec15535a06f0f561329b14ea99322a97d520b3
+size 85
